1410b44e50934451281521632ee46e345643ddb6
郭宇沫 <damozi@guoyumodeMacBook-Pro.local>
2018-07-14 15:53:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="4">
+        <div class="el-menu--horizontal h-60">
+          <img src="./assets/logo.png" height="60px"/>
+        </div>
+      </el-col>
+      <el-col :span="20">
+        <elmenu></elmenu>
+      </el-col>
+    </el-row>
+    <el-row>
+      <router-view/>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import elmenu from './components/menu.vue'
+export default {
+  name: 'app',
+  components: {
+    elmenu
+  },
+  mounted () {
+    console.log(this.$route)
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.h-60 {
+  height: 60px;
+}
+.position-fixed{
+  position: fixed;
+  left: 0;
+  right: 0;
+}
+.margin-60{
+  margin-top:60px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      activeIndex: '1'
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
